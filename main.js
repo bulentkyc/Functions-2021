a59720ecd3c0626b787895e5bb6b8d849f789cce
BULENT KAYICI <bulent@Bulents-Mac-mini.fritz.box>
2021-12-14 11:36:54 +0100

    v03

--- main.js
+++ main.js
@@ -45,17 +45,75 @@ if (isNaN(age)) {
 
 
 function sum(number1, number2) {
-    if (isNaN(number2)) {
-        console.log('You should give me the second number');
+    if (isNaN(number1) || isNaN(number2)) {
+        return('You should give me only numbers');
     } else {
         var total = number1 + number2;
-        console.log(total);
+        return(total);
+        alert('test') // After return no code works
     }
 }
 
+var n1;
 function promptSum() {
-    var number1 = parseInt(prompt(`1. number: `));
+    n1 = parseInt(prompt(`1. number: `));
     var n2 = parseInt(prompt(`2. number: `));
     
-    sum(number1, n2);
+    alert(sum(n1, n2));
 }
+
+console.log(n1);
+
+var testX = 5
+
+var tester;
+
+tester = testX; // tester = 5
+
+console.log( sum() );
+console.log( sum );
+
+
+function isPrime1(cNumber) {
+    var result;
+    var i;
+    for (i = 2; i < cNumber/2; i++) {
+        console.log(`${cNumber}/${i}=${cNumber/i}`);
+        if (cNumber % i === 0) {
+            result = false;
+            //console.log(`${cNumber} is not a prime number, since it's dividable by ${i}`);
+            break;
+        }
+    }
+    console.log(i);
+    if (Math.ceil(cNumber/2) === i) {
+        result = true;
+        //console.log(`${cNumber} is a prime number.`);
+    }
+    return result;
+}
+
+function isPrime2(cNumber) {
+    for (let i = 2; i < cNumber/2; i++) {
+        //console.log(`${cNumber}/${i}=${cNumber/i}`);
+        if (cNumber % i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPrime2(751));
+
+
+
+
+
+/* 
+    //if we hav e numbers
+    if (condition) {
+        
+    }
+ */
+    
+    //Loop through numbers start and end
