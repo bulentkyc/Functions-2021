97467909e95eaec0e76d15413b5f542acc8f56d9
BULENT KAYICI <bulent@Bulents-Mac-mini.fritz.box>
2022-01-04 13:34:03 +0100

    v2

--- lastLesson.js
+++ lastLesson.js
@@ -1,3 +1,20 @@
-const promptGetLonger = () => {
-    console.log('test');
+const run = () => {
+    console.log('Clicked');
+    sum(4);
+    sum(1,5);
+    sum();
+    salute('Essence');
+    salute();
+}
+
+//run() // Doesn't work since hoisting precent iwit const
+
+const sum = (number1 = 0, number2 = 0) => {
+    console.log(number1);
+    console.log(number2);
+    console.log(number1+number2);
+}
+
+const salute = (name = 'Alien') => {
+    alert(`Hi ${name}`);
 }
