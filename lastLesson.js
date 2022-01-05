4bb8aac650b272c183b558b909f1cbd9132f9432
BULENT KAYICI <bulent@Bulents-Mini.fritz.box>
2022-01-05 09:08:58 +0100

    v2

--- lastLesson.js
+++ lastLesson.js
@@ -1,3 +1,5 @@
+// Default values of parameters
+
 const run = () => {
     console.log('Clicked');
     sum(4);
@@ -18,3 +20,33 @@ const sum = (number1 = 0, number2 = 0) => {
 const salute = (name = 'Alien') => {
     alert(`Hi ${name}`);
 }
+
+
+//Recursive Functions
+
+const countDown = (startingNumber) => {
+    //startingNumber = startingNumber -1;
+    //console.log(startingNumber--);
+    console.log(startingNumber);
+    startingNumber--;
+    
+    if (startingNumber >= 0) {
+        countDown(startingNumber);
+    }
+}
+
+countDown(10);
+
+// Rest params
+
+const listMyGarage = (car1, car2, ...others) => {
+    console.log(car1, car2, others);
+}
+
+/*
+const listMyGarage = (...others) => {
+    console.log(others);
+}
+*/
+
+listMyGarage('BMW', 'Mercedes', 'Porsche', 'Ferrari', 'Bugatti');
